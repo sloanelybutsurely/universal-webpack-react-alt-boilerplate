f8393994c4f9904e0bedee43aabecd8b62ea9e32
Zach Perrault <zach.perrault@gmail.com>
2015-08-29 12:17:34 -0400

    Updated npm scripts and some more things

--- src/client.js
+++ src/client.js
@@ -1,10 +1,8 @@
 import React from 'react'
-import Router from 'react-router'
+import Router, {HistoryLocation} from 'react-router'
 
 import routes from 'routes'
 
-document.addEventListener('DOMContentLoaded', () => {
-  Router.run(routes, (Handler) => {
-    React.render(<Handler />, document.getElementById('app'))
-  })
+Router.run(routes, HistoryLocation, (Handler) => {
+  React.render(<Handler />, document.getElementById('app'))
 })
